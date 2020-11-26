5493a6aa47c95bdb610976c429e00bba32003c99
Julien Tromas <julien.tromas@gmail.com>
2020-11-26 11:18:34 +0900

    Connection established between frontend and backend. Able to display info on the profile page. Still need to look into the UI to be able to display more informations AND keep the buttons on the page

--- code_and_sorcery/lib/screens/user_profile/user_profile.dart
+++ code_and_sorcery/lib/screens/user_profile/user_profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class UserProfile extends StatelessWidget {
   @override
@@ -7,25 +8,80 @@ class UserProfile extends StatelessWidget {
         appBar: AppBar(
           title: Text("Profile page"),
         ),
-        body:
-        new Column(children: <Widget>[
-          ElevatedButton(
-            onPressed: () {
-              // Navigate back to the first screen by popping the current route
-              // off the stack.
-              Navigator.pushNamed(context, '/guild');
-            },
-            child: Text('Go to guild view'),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              // Navigate back to the first screen by popping the current route
-              // off the stack.
-              Navigator.pop(context);
-            },
-            child: Text('Go to homepage'),
-          ),
-        ])
+        body:_buildBody(context)
+        // Column(children: <Widget>[
+        //   ElevatedButton(
+        //     onPressed: () {
+        //       Navigator.pushNamed(context, '/guild');
+        //     },
+        //     child: Text('Go to guild view'),
+        //   ),
+        //   ElevatedButton(
+        //     onPressed: () {
+        //       Navigator.pop(context);
+        //     },
+        //     child: Text('Go to homepage'),
+        //   ),
+        //
+        // ]),
     );
   }
 }
+
+Widget _buildBody(BuildContext context) {
+  return StreamBuilder<QuerySnapshot>(
+    stream: Firestore.instance.collection('users').snapshots(),
+    builder: (context, snapshot) {
+      if (!snapshot.hasData) return LinearProgressIndicator();
+
+      return _buildList(context, snapshot.data.documents);
+    },
+  );
+}
+
+
+Widget _buildList(BuildContext context, List<DocumentSnapshot> snapshot) {
+  return ListView(
+    padding: const EdgeInsets.only(top: 20.0),
+    children: snapshot.map((data) => _buildListItem(context, data)).toList(),
+  );
+}
+
+Widget _buildListItem(BuildContext context, DocumentSnapshot data) {
+  final record = Record.fromSnapshot(data);
+
+  return Padding(
+    key: ValueKey(record.username),
+    padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+    child: Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(5.0),
+      ),
+      child: ListTile(
+        title: Text(record.username),
+        trailing: Text(record.points.toString()),
+        onTap: () => print(record),
+      ),
+    ),
+  );
+}
+class Record {
+  final String username;
+  final int points;
+  final DocumentReference reference;
+
+  Record.fromMap(Map<String, dynamic> map, {this.reference})
+      : assert(map['username'] != null),
+        assert(map['points'] != null),
+        username = map['username'],
+        points = map['points'];
+
+
+  Record.fromSnapshot(DocumentSnapshot snapshot)
+      : this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  @override
+  String toString() => "Record<$username:$points>";
+}
+
