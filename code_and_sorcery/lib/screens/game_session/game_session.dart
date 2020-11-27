ca1473e2975c64dffe65f5366f9ac15700dca156
Nina Matti <ninadesireematti@gmail.com>
2020-11-27 14:57:05 +0900

    friday afternoon version

--- code_and_sorcery/lib/screens/game_session/game_session.dart
+++ code_and_sorcery/lib/screens/game_session/game_session.dart
@@ -1,5 +1,247 @@
 import 'package:flutter/material.dart';
 
+
+
+// class GameContent{
+//   var images = ["monster1", "monster2", "monster3", "boss1"];
+//
+//   var questions = [
+//     "What does JS stand for?",
+//     "What is Vue.js?",
+//     "Best JavaScript library ever?",
+//     "How do you print to the console in JS?"
+//   ];
+//
+//   var choices = [
+//     ["JesusSighs", "Justice served", "JavaScript", "Just subtleties"],
+//     ["Encoder", "Framework", "Language", "Library"],
+//     ["React", "Vue", "Angular", "Underscore"],
+//     ["console.log()", "print()", "login.Debug();", "Console.WriteLine();"]
+//   ];
+//
+//   var correctAnswers = [
+//     "JavaScript", "Library", "Vue", "console.log()"
+//   ];
+// }
+//
+// // game variables
+// var finalScore = 0;
+// var questionNumber = 0;
+// // variable that holds game object:
+// var game = new GameContent();
+//
+// // Game widget class
+// class Game1 extends StatefulWidget {
+//   @override
+//   State<StatefulWidget> createState() {
+//     return new Game1State();
+//   }
+// }
+//
+// // Game widget state
+// class Game1State extends State<Game1>{
+//   @override
+//   Widget build(BuildContext context) {
+//     return new WillPopScope(
+//         onWillPop: ()async => false,
+//         child: Scaffold(
+//
+//             // body
+//             body: new Container(
+//                 margin: const EdgeInsets.all(10.0),
+//                 alignment: Alignment.topCenter,
+//                 child: new Column(
+//                     children: <Widget>[
+//                       Padding(padding: EdgeInsets.all(10.0)),
+//
+//                       // top row that displays question number and current score
+//                       Container(
+//                           alignment: Alignment.centerRight,
+//                           child: Row(
+//                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                               children: <Widget>[
+//
+//                                 Text("Question ${questionNumber + 1} of ${game.questions.length}",
+//                                   style: TextStyle(
+//                                       fontSize: 22.0
+//                                   ),),
+//
+//                                 Text("Score: $finalScore",
+//                                   style: TextStyle(
+//                                       fontSize: 22.0
+//                                   ),),
+//                               ]
+//                           )
+//                       ),
+//
+//                       // image
+//                       Padding(padding: EdgeInsets.all(10.0)),
+//
+//                       new Image.asset(
+//                           "images/${game.images[questionNumber]}.jpg"
+//                       ),
+//
+//                       Padding(padding: EdgeInsets.all(10.0)),
+//
+//                       // question
+//                       Text(quiz.questions[questionNumber],
+//                         style: TextStyle(
+//                           fontSize: 20.0,
+//                         ),),
+//
+//                       Padding(padding: EdgeInsets.all(10.0),),
+//
+//                       // answers row 1
+//                       Row(
+//                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                           children: <Widget>[
+//
+//                             //button 1
+//                             MaterialButton(
+//                               minWidth: 120.0,
+//                               color: Colors.blueGrey,
+//                               onPressed: () {
+//                                 if(quiz.choices[questionNumber][0] == quiz.correctAnswers[questionNumber]) {
+//                                   debugPrint('correctamundo');
+//                                   finalScore++;
+//                                 } else {
+//                                   debugPrint('oh noes... that is incorrect');
+//                                 }
+//                                 updateQuestion();
+//                               },
+//                               child: Text(quiz.choices[questionNumber][0],
+//                                 style: TextStyle(
+//                                   fontSize: 20.0,
+//                                   color: Colors.white,
+//                                 ),
+//                               ),
+//                             ),
+//
+//                             // button 2
+//                             MaterialButton(
+//                               minWidth: 120.0,
+//                               color: Colors.blueGrey,
+//                               onPressed: () {
+//                                 if(quiz.choices[questionNumber][1] == quiz.correctAnswers[questionNumber]) {
+//                                   debugPrint('correctamundo');
+//                                   finalScore++;
+//                                 } else {
+//                                   debugPrint('oh noes... that is incorrect');
+//                                 }
+//                                 updateQuestion();
+//                               },
+//                               child: Text(quiz.choices[questionNumber][1],
+//                                 style: TextStyle(
+//                                   fontSize: 20.0,
+//                                   color: Colors.white,
+//                                 ),
+//                               ),
+//                             ),
+//
+//                           ]
+//                       ),
+//
+//                       Padding(padding: EdgeInsets.all(10.0),),
+//
+//                       // answers row 2
+//                       Row(
+//                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                           children: <Widget>[
+//
+//                             //button 3
+//                             MaterialButton(
+//                               minWidth: 120.0,
+//                               color: Colors.blueGrey,
+//                               onPressed: () {
+//                                 if(quiz.choices[questionNumber][2] == quiz.correctAnswers[questionNumber]) {
+//                                   debugPrint('correctamundo');
+//                                   finalScore++;
+//                                 } else {
+//                                   debugPrint('oh noes... that is incorrect');
+//                                 }
+//                                 updateQuestion();
+//                               },
+//                               child: Text(quiz.choices[questionNumber][2],
+//                                 style: TextStyle(
+//                                   fontSize: 20.0,
+//                                   color: Colors.white,
+//                                 ),
+//                               ),
+//                             ),
+//
+//                             // button 4
+//                             MaterialButton(
+//                               minWidth: 120.0,
+//                               color: Colors.blueGrey,
+//                               onPressed: () {
+//                                 if(quiz.choices[questionNumber][3] == quiz.correctAnswers[questionNumber]) {
+//                                   debugPrint('correctamundo');
+//                                   finalScore++;
+//                                 } else {
+//                                   debugPrint('oh noes... that is incorrect');
+//                                 }
+//                                 updateQuestion();
+//                               },
+//                               child: Text(quiz.choices[questionNumber][3],
+//                                 style: TextStyle(
+//                                   fontSize: 20.0,
+//                                   color: Colors.white,
+//                                 ),
+//                               ),
+//                             ),
+//
+//                           ]
+//                       ),
+//
+//                       Padding(padding: EdgeInsets.all(10.0),),
+//
+//                       // reset button
+//                       Container(
+//                           alignment: Alignment.bottomCenter,
+//                           child: MaterialButton(
+//                             color: Colors.redAccent,
+//                             minWidth: 240.0,
+//                             height: 30.0,
+//                             onPressed: resetQuiz,
+//                             child: Text("Quit",
+//                               style: TextStyle(
+//                                 fontSize: 18.0,
+//                                 color: Colors.white,
+//                               ),
+//                             ),
+//                           )
+//                       )
+//                     ]
+//                 )
+//             )
+//         )
+//     );
+//   }
+//
+//   // resetting question/answer screen
+//   void resetQuiz() {
+//     setState(() {
+//       // close current screen:
+//       Navigator.pop(context);
+//       // reset variables:
+//       finalScore = 0;
+//       questionNumber = 0;
+//     });
+//   }
+//
+//   // changing to new question OR go to leaderboard if last question
+//   void updateQuestion() {
+//     setState(() {
+//       if(questionNumber == quiz.questions.length -1){
+//         Navigator.push(context, MaterialPageRoute(builder: (context) => new Summary(score: finalScore)));
+//       } else {
+//         questionNumber++;
+//       }
+//     });
+//   }
+// }
+
+
 class InGame extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
