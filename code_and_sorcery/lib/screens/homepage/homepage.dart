dccd09d48166b8f6e641bc8128bf4866d62f208d
tlightk <klayxi@gmail.com>
2020-12-01 14:41:41 +0900

    two players can join the same game lobby

--- code_and_sorcery/lib/screens/homepage/homepage.dart
+++ code_and_sorcery/lib/screens/homepage/homepage.dart
@@ -5,7 +5,7 @@ import '../login/authenticator.dart';
 import '../login/login.dart';
 
 var profileImg = 'https://i.pinimg.com/originals/f3/be/e8/f3bee827c8aee1048d84bbb02af2e6b6.jpg';
-var userName = 'Clay';
+// var userName = 'Clay';
 
 
 
@@ -109,7 +109,7 @@ class Homepage extends StatelessWidget {
         .set({
       'created': FieldValue.serverTimestamp(),
       'finished': false,
-      'player1': (userName),
+      'player1': username,
       'player1Points': 0,
       'player2': '',
       'player2Points': 0,
