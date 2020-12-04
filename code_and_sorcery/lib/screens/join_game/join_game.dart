2729a89bdb08797cd9bf792677f0c9bdd7ab67b3
Julien Tromas <julien.tromas@gmail.com>
2020-12-04 14:29:11 +0900

    Fixed some issues with global variables

--- code_and_sorcery/lib/screens/join_game/join_game.dart
+++ code_and_sorcery/lib/screens/join_game/join_game.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../global_variables/global_variables.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../game_lobby/game_lobby.dart';
 
 String player2db;
 String player3db;
@@ -76,7 +75,7 @@ class JoinGameState extends State<JoinGame> {
   void setPlayer() async {
     await _firestore.runTransaction((transaction) async {
       DocumentReference playerCheck =
-          _firestore.collection('games').doc(gameLinkValue);
+          _firestore.collection('games').doc(gameLink);
       DocumentSnapshot snapshot = await transaction.get(playerCheck);
       player2db = snapshot.data()['player2'];
       player3db = snapshot.data()['player3'];
@@ -94,45 +93,4 @@ class JoinGameState extends State<JoinGame> {
       }
     });
   }
-
-  // void setPlayer3() async {
-  //   await databaseReference.collection("games").doc(gameLink).update({
-  //     'player3': username,
-  //   });
-  // }
-
-  // void setPlayer4() async {
-  //   await databaseReference.collection("games").doc(gameLink).update({
-  //     'player4': username,
-  //   });
-  // }
-}
-
-/*
-FOUR PLAYER LOGIC :
-
-Check if PLAYER2 is empty or not in the game object
-  IF PLAYER2 is empty, set CURRENTPLAYER as PLAYER2
-  ELSE : IF PLAYER2 is full, check if PLAYER3 is empty or not in the game object
-            IF PLAYER3 is empty, set CURRENTPLAYER as PLAYER3
-            ELSE : IF PLAYER 3 is full, check if PLAYER4 is empty or not in the game object
-                      IF PLAYER4 is empty, set CURRENTPLAYER as PLAYER4
-                      ELSE : IF PLAYER4 is full, send alert message in popup box saying the game is already full
-*/
-class Record {
-  final String player2;
-  final String player3;
-  final String player4;
-  final DocumentReference reference;
-
-  Record.fromMap(Map<String, dynamic> map, {this.reference})
-      : assert(map['player2'] != null),
-        assert(map['player3'] != null),
-        assert(map['player4'] != null),
-        player2 = map['player2'],
-        player3 = map['player3'],
-        player4 = map['player4'];
-
-  @override
-  String toString() => "Record<$player2:$player3:$player4>";
 }
