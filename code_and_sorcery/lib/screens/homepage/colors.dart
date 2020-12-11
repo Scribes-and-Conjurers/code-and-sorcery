36a6916132ab845414e9c5fad69b444f2f7b6b6c
Nina Matti <ninadesireematti@gmail.com>
2020-12-12 00:38:16 +0900

    Added two more color palettes

--- code_and_sorcery/lib/screens/homepage/colors.dart
+++ code_and_sorcery/lib/screens/homepage/colors.dart
@@ -1,66 +1,202 @@
 import 'package:flutter/material.dart';
 
+// First color scheme/ Palette 1:
+// Color 1
+// Map<int, Color> colorBase1 = {
+//   50: Color.fromRGBO(47, 37, 25, .1),
+//   100: Color.fromRGBO(47, 37, 25, .2),
+//   200: Color.fromRGBO(47, 37, 25, .3),
+//   300: Color.fromRGBO(47, 37, 25, .4),
+//   400: Color.fromRGBO(47, 37, 25, .5),
+//   500: Color.fromRGBO(47, 37, 25, .6),
+//   600: Color.fromRGBO(47, 37, 25, .7),
+//   700: Color.fromRGBO(47, 37, 25, .8),
+//   800: Color.fromRGBO(47, 37, 25, .9),
+//   900: Color.fromRGBO(47, 37, 25, 1),
+// };
+
+// MaterialColor colorMain1 = MaterialColor(0xFF2F2519, colorBase1);
+
+// // Color 2
+// Map<int, Color> colorBase2 = {
+//   50: Color.fromRGBO(74, 63, 53, .1),
+//   100: Color.fromRGBO(74, 63, 53, .2),
+//   200: Color.fromRGBO(74, 63, 53, .3),
+//   300: Color.fromRGBO(74, 63, 53, .4),
+//   400: Color.fromRGBO(74, 63, 53, .5),
+//   500: Color.fromRGBO(74, 63, 53, .6),
+//   600: Color.fromRGBO(74, 63, 53, .7),
+//   700: Color.fromRGBO(74, 63, 53, .8),
+//   800: Color.fromRGBO(74, 63, 53, .9),
+//   900: Color.fromRGBO(74, 63, 53, 1),
+// };
+
+// MaterialColor colorMain2 = MaterialColor(0xFF4a3f35, colorBase2);
+
+// // Color 3
+// Map<int, Color> colorBase3 = {
+//   50: Color.fromRGBO(250, 125, 9, .1),
+//   100: Color.fromRGBO(250, 125, 9, .2),
+//   200: Color.fromRGBO(250, 125, 9, .3),
+//   300: Color.fromRGBO(250, 125, 9, .4),
+//   400: Color.fromRGBO(250, 125, 9, .5),
+//   500: Color.fromRGBO(250, 125, 9, .6),
+//   600: Color.fromRGBO(250, 125, 9, .7),
+//   700: Color.fromRGBO(250, 125, 9, .8),
+//   800: Color.fromRGBO(250, 125, 9, .9),
+//   900: Color.fromRGBO(250, 125, 9, 1),
+// };
+
+// MaterialColor colorSide1 = MaterialColor(0xFFfa7d09, colorBase3);
+
+// // Color 4
+// Map<int, Color> colorBase4 = {
+//   50: Color.fromRGBO(255, 67, 1, .1),
+//   100: Color.fromRGBO(255, 67, 1, .2),
+//   200: Color.fromRGBO(255, 67, 1, .3),
+//   300: Color.fromRGBO(255, 67, 1, .4),
+//   400: Color.fromRGBO(255, 67, 1, .5),
+//   500: Color.fromRGBO(255, 67, 1, .6),
+//   600: Color.fromRGBO(255, 67, 1, .7),
+//   700: Color.fromRGBO(255, 67, 1, .8),
+//   800: Color.fromRGBO(255, 67, 1, .9),
+//   900: Color.fromRGBO(255, 67, 1, 1),
+// };
+
+// MaterialColor colorSide2 = MaterialColor(0xFFff4301, colorBase4);
+
+// ===========================================================================
+
+// Palette 2
 // First color scheme:
 // Color 1
+// Map<int, Color> colorBase1 = {
+//   50: Color.fromRGBO(238, 238, 238, .1),
+//   100: Color.fromRGBO(238, 238, 238, .2),
+//   200: Color.fromRGBO(238, 238, 238, .3),
+//   300: Color.fromRGBO(238, 238, 238, .4),
+//   400: Color.fromRGBO(238, 238, 238, .5),
+//   500: Color.fromRGBO(238, 238, 238, .6),
+//   600: Color.fromRGBO(238, 238, 238, .7),
+//   700: Color.fromRGBO(238, 238, 238, .8),
+//   800: Color.fromRGBO(238, 238, 238, .9),
+//   900: Color.fromRGBO(238, 238, 238, 1),
+// };
+
+// MaterialColor colorMain1 = MaterialColor(0xFFeeeeee, colorBase1);
+
+// // Color 2
+// Map<int, Color> colorBase2 = {
+//   50: Color.fromRGBO(104, 109, 118, .1),
+//   100: Color.fromRGBO(104, 109, 118, .2),
+//   200: Color.fromRGBO(104, 109, 118, .3),
+//   300: Color.fromRGBO(104, 109, 118, .4),
+//   400: Color.fromRGBO(104, 109, 118, .5),
+//   500: Color.fromRGBO(104, 109, 118, .6),
+//   600: Color.fromRGBO(104, 109, 118, .7),
+//   700: Color.fromRGBO(104, 109, 118, .8),
+//   800: Color.fromRGBO(104, 109, 118, .9),
+//   900: Color.fromRGBO(104, 109, 118, 1),
+// };
+
+// MaterialColor colorMain2 = MaterialColor(0xFF686d76, colorBase2);
+
+// // Color 3
+// Map<int, Color> colorBase3 = {
+//   50: Color.fromRGBO(55, 58, 64, .1),
+//   100: Color.fromRGBO(55, 58, 64, .2),
+//   200: Color.fromRGBO(55, 58, 64, .3),
+//   300: Color.fromRGBO(55, 58, 64, .4),
+//   400: Color.fromRGBO(55, 58, 64, .5),
+//   500: Color.fromRGBO(55, 58, 64, .6),
+//   600: Color.fromRGBO(55, 58, 64, .7),
+//   700: Color.fromRGBO(55, 58, 64, .8),
+//   800: Color.fromRGBO(55, 58, 64, .9),
+//   900: Color.fromRGBO(55, 58, 64, 1),
+// };
+
+// MaterialColor colorSide1 = MaterialColor(0xFF373a40, colorBase3);
+
+// // Color 4
+// Map<int, Color> colorBase4 = {
+//   50: Color.fromRGBO(25, 211, 218, .1),
+//   100: Color.fromRGBO(25, 211, 218, .2),
+//   200: Color.fromRGBO(25, 211, 218, .3),
+//   300: Color.fromRGBO(25, 211, 218, .4),
+//   400: Color.fromRGBO(25, 211, 218, .5),
+//   500: Color.fromRGBO(25, 211, 218, .6),
+//   600: Color.fromRGBO(25, 211, 218, .7),
+//   700: Color.fromRGBO(25, 211, 218, .8),
+//   800: Color.fromRGBO(25, 211, 218, .9),
+//   900: Color.fromRGBO(25, 211, 218, 1),
+// };
+
+// MaterialColor colorSide2 = MaterialColor(0xFF19d3da, colorBase4);
+
+// ===========================================================================
+
+// Palette 3
+
+// Color 1
 Map<int, Color> colorBase1 = {
-  50: Color.fromRGBO(47, 37, 25, .1),
-  100: Color.fromRGBO(47, 37, 25, .2),
-  200: Color.fromRGBO(47, 37, 25, .3),
-  300: Color.fromRGBO(47, 37, 25, .4),
-  400: Color.fromRGBO(47, 37, 25, .5),
-  500: Color.fromRGBO(47, 37, 25, .6),
-  600: Color.fromRGBO(47, 37, 25, .7),
-  700: Color.fromRGBO(47, 37, 25, .8),
-  800: Color.fromRGBO(47, 37, 25, .9),
-  900: Color.fromRGBO(47, 37, 25, 1),
+  50: Color.fromRGBO(241, 241, 241, .1),
+  100: Color.fromRGBO(241, 241, 241, .2),
+  200: Color.fromRGBO(241, 241, 241, .3),
+  300: Color.fromRGBO(241, 241, 241, .4),
+  400: Color.fromRGBO(241, 241, 241, .5),
+  500: Color.fromRGBO(241, 241, 241, .6),
+  600: Color.fromRGBO(241, 241, 241, .7),
+  700: Color.fromRGBO(241, 241, 241, .8),
+  800: Color.fromRGBO(241, 241, 241, .9),
+  900: Color.fromRGBO(241, 241, 241, 1),
 };
 
-MaterialColor colorMain1 = MaterialColor(0xFF2F2519, colorBase1);
+MaterialColor colorMain1 = MaterialColor(0xFFf1f1f1, colorBase1);
 
 // Color 2
 Map<int, Color> colorBase2 = {
-  50: Color.fromRGBO(74, 63, 53, .1),
-  100: Color.fromRGBO(74, 63, 53, .2),
-  200: Color.fromRGBO(74, 63, 53, .3),
-  300: Color.fromRGBO(74, 63, 53, .4),
-  400: Color.fromRGBO(74, 63, 53, .5),
-  500: Color.fromRGBO(74, 63, 53, .6),
-  600: Color.fromRGBO(74, 63, 53, .7),
-  700: Color.fromRGBO(74, 63, 53, .8),
-  800: Color.fromRGBO(74, 63, 53, .9),
-  900: Color.fromRGBO(74, 63, 53, 1),
+  50: Color.fromRGBO(253, 184, 39, .1),
+  100: Color.fromRGBO(253, 184, 39, .2),
+  200: Color.fromRGBO(253, 184, 39, .3),
+  300: Color.fromRGBO(253, 184, 39, .4),
+  400: Color.fromRGBO(253, 184, 39, .5),
+  500: Color.fromRGBO(253, 184, 39, .6),
+  600: Color.fromRGBO(253, 184, 39, .7),
+  700: Color.fromRGBO(253, 184, 39, .8),
+  800: Color.fromRGBO(253, 184, 39, .9),
+  900: Color.fromRGBO(253, 184, 39, 1),
 };
 
-MaterialColor colorMain2 = MaterialColor(0xFF4a3f35, colorBase2);
+MaterialColor colorMain2 = MaterialColor(0xFFfdb827, colorBase2);
 
 // Color 3
 Map<int, Color> colorBase3 = {
-  50: Color.fromRGBO(250, 125, 9, .1),
-  100: Color.fromRGBO(250, 125, 9, .2),
-  200: Color.fromRGBO(250, 125, 9, .3),
-  300: Color.fromRGBO(250, 125, 9, .4),
-  400: Color.fromRGBO(250, 125, 9, .5),
-  500: Color.fromRGBO(250, 125, 9, .6),
-  600: Color.fromRGBO(250, 125, 9, .7),
-  700: Color.fromRGBO(250, 125, 9, .8),
-  800: Color.fromRGBO(250, 125, 9, .9),
-  900: Color.fromRGBO(250, 125, 9, 1),
+  50: Color.fromRGBO(33, 32, 156, .1),
+  100: Color.fromRGBO(33, 32, 156, .2),
+  200: Color.fromRGBO(33, 32, 156, .3),
+  300: Color.fromRGBO(33, 32, 156, .4),
+  400: Color.fromRGBO(33, 32, 156, .5),
+  500: Color.fromRGBO(33, 32, 156, .6),
+  600: Color.fromRGBO(33, 32, 156, .7),
+  700: Color.fromRGBO(33, 32, 156, .8),
+  800: Color.fromRGBO(33, 32, 156, .9),
+  900: Color.fromRGBO(33, 32, 156, 1),
 };
 
-MaterialColor colorSide1 = MaterialColor(0xFFfa7d09, colorBase3);
+MaterialColor colorSide1 = MaterialColor(0xFF21209c, colorBase3);
 
 // Color 4
 Map<int, Color> colorBase4 = {
-  50: Color.fromRGBO(255, 67, 1, .1),
-  100: Color.fromRGBO(255, 67, 1, .2),
-  200: Color.fromRGBO(255, 67, 1, .3),
-  300: Color.fromRGBO(255, 67, 1, .4),
-  400: Color.fromRGBO(255, 67, 1, .5),
-  500: Color.fromRGBO(255, 67, 1, .6),
-  600: Color.fromRGBO(255, 67, 1, .7),
-  700: Color.fromRGBO(255, 67, 1, .8),
-  800: Color.fromRGBO(255, 67, 1, .9),
-  900: Color.fromRGBO(255, 67, 1, 1),
+  50: Color.fromRGBO(35, 18, 11, .1),
+  100: Color.fromRGBO(35, 18, 11, .2),
+  200: Color.fromRGBO(35, 18, 11, .3),
+  300: Color.fromRGBO(35, 18, 11, .4),
+  400: Color.fromRGBO(35, 18, 11, .5),
+  500: Color.fromRGBO(35, 18, 11, .6),
+  600: Color.fromRGBO(35, 18, 11, .7),
+  700: Color.fromRGBO(35, 18, 11, .8),
+  800: Color.fromRGBO(35, 18, 11, .9),
+  900: Color.fromRGBO(35, 18, 11, 1),
 };
 
-MaterialColor colorSide2 = MaterialColor(0xFFff4301, colorBase4);
+MaterialColor colorSide2 = MaterialColor(0xFF23120b, colorBase4);
