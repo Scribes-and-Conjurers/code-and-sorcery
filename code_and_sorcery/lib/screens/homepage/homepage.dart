e1e9702be7bbfef5f80f2d7a59b1fcec86cb0ed8
Nina Matti <ninadesireematti@gmail.com>
2020-12-12 17:03:06 +0900

    updated debugged homepage

--- code_and_sorcery/lib/screens/homepage/homepage.dart
+++ code_and_sorcery/lib/screens/homepage/homepage.dart
@@ -136,7 +136,6 @@ class Homepage extends StatelessWidget {
                   child: Text('Profile page'),
                 ),
               ),
-
               SizedBox(
                 width: 200.0,
                 child: ElevatedButton(
@@ -411,24 +410,6 @@ class Homepage extends StatelessWidget {
         });
   }
 
-
-extension HexColor on Color {
-  /// String is in the format "aabbcc" or "ffaabbcc" with an optional leading "#".
-  static Color fromHex(String hexString) {
-    final buffer = StringBuffer();
-    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
-    buffer.write(hexString.replaceFirst('#', ''));
-    return Color(int.parse(buffer.toString(), radix: 16));
-  }
-
-  /// Prefixes a hash sign if [leadingHashSign] is set to `true` (default is `true`).
-  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
-      '${alpha.toRadixString(16).padLeft(2, '0')}'
-      '${red.toRadixString(16).padLeft(2, '0')}'
-      '${green.toRadixString(16).padLeft(2, '0')}'
-      '${blue.toRadixString(16).padLeft(2, '0')}';
-}
-
 // void gameFullCheck() async {
 //   await FirebaseFirestore.instance.runTransaction((transaction) async {
 //     DocumentReference playerCheck =
@@ -455,7 +436,6 @@ extension HexColor on Color {
           );
         });
   }
-
 
   Future<String> alertGameStarted(BuildContext context) {
     return showDialog(
