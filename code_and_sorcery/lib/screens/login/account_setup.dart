f6392100e57ae2c95bacd88e4b60cc3a8492986e
tlightk <klayxi@gmail.com>
2020-12-13 15:04:01 +0900

    fixed the overflow bug on account setup and changed some text

--- code_and_sorcery/lib/screens/login/account_setup.dart
+++ code_and_sorcery/lib/screens/login/account_setup.dart
@@ -24,138 +24,139 @@ class AccountSetupState extends State<AccountSetup> {
 
   @override
   Widget build(BuildContext ctxt) {
-    return new MaterialApp(
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: widget.title,
-        ),
-        body: Column(
-          children: <Widget>[
-            SizedBox(height: 30),
-            Text("Choose your username"),
-            SizedBox(height: 30),
-            TextField(
-              controller: usernameController,
-              decoration: new InputDecoration(
-                  border: OutlineInputBorder(), hintText: "Type your username"),
-              onChanged: (String text) {
-                setState(() {
-                  usernameValue = usernameController.text;
-                  print('usernameValue $usernameValue');
-                  print('test');
-                  username = usernameValue;
-                });
-              },
-            ),
-            SizedBox(height: 50),
-            Text("Choose your guild"),
-            SizedBox(height: 30),
-            DropdownButton<String>(
-              value: "Backenders",
-              icon: Icon(Icons.arrow_downward),
-              iconSize: 24,
-              elevation: 16,
-              style: TextStyle(color: Colors.deepPurple),
-              underline: Container(
-                height: 2,
-                color: Colors.deepPurpleAccent,
-              ),
-              onChanged: (String newValue) {
-                setState(() {
-                  dropdownGuildValue = newValue;
-                  guild = dropdownGuildValue;
-                });
-              },
-              items: <String>['Backenders', 'Frontenders', 'Fullstackers']
-                  .map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
-            SizedBox(height: 50),
-            Text("Choose your class"),
-            SizedBox(height: 30),
-            Container(
-              height: 64,
-              width: 100,
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.black, width: 1),
-                color: Colors.orange,
-              ),
-              child: TextButton(
-                style: TextButton.styleFrom(primary: Colors.transparent),
-                onPressed: () {
-                  switch (playerClass) {
-                    case 'Warrior':
-                      {
-                        setState(() {
-                          playerClass = 'Wizard';
-                          print('Wizard');
-                        });
-                      }
-                      break;
-                    case 'Wizard':
-                      {
-                        setState(() {
-                          playerClass = 'Warrior';
-                          print('Warrior');
-                        });
-                      }
-                      break;
-                    default:
-                      {
-                        setState(() {
-                          playerClass = 'Warrior';
-                        });
-                      }
-                      break;
-                  }
+    return new WillPopScope(
+      onWillPop: () async => false,
+      child: new Scaffold(
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 64),
+              Text("Choose your username"),
+              SizedBox(height: 30),
+              TextField(
+                controller: usernameController,
+                decoration: new InputDecoration(
+                    border: OutlineInputBorder(), hintText: "Type your username"),
+                onChanged: (String text) {
+                  setState(() {
+                    usernameValue = usernameController.text;
+                    print('usernameValue $usernameValue');
+                    print('test');
+                    username = usernameValue;
+                  });
                 },
-                child: Text(
-                  playerClass,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black,
+              ),
+              SizedBox(height: 50),
+              Text("Choose your guild"),
+              SizedBox(height: 30),
+              DropdownButton<String>(
+                value: "Backenders",
+                icon: Icon(Icons.arrow_downward),
+                iconSize: 24,
+                elevation: 16,
+                style: TextStyle(color: Colors.deepPurple),
+                underline: Container(
+                  height: 2,
+                  color: Colors.deepPurpleAccent,
+                ),
+                onChanged: (String newValue) {
+                  setState(() {
+                    dropdownGuildValue = newValue;
+                    guild = dropdownGuildValue;
+                  });
+                },
+                items: <String>['Backenders', 'Frontenders', 'Fullstackers']
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 50),
+              Text("Class:"),
+              SizedBox(height: 30),
+              Container(
+                height: 64,
+                width: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 1),
+                  color: Colors.orange,
+                ),
+                child: TextButton(
+                  style: TextButton.styleFrom(primary: Colors.transparent),
+                  onPressed: () {
+                    switch (playerClass) {
+                      case 'Warrior':
+                        {
+                          setState(() {
+                            playerClass = 'Wizard';
+                            print('Wizard');
+                          });
+                        }
+                        break;
+                      case 'Wizard':
+                        {
+                          setState(() {
+                            playerClass = 'Warrior';
+                            print('Warrior');
+                          });
+                        }
+                        break;
+                      default:
+                        {
+                          setState(() {
+                            playerClass = 'Warrior';
+                          });
+                        }
+                        break;
+                    }
+                  },
+                  child: Text(
+                    playerClass,
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(height: 30),
-            Container(
-              height: 50,
-              width: 300,
-              // decoration: BoxDecoration(
-              //   border: Border.all(color: Colors.black, width: 1),
-              // ),
-              child: Text((() {
-                if (playerClass == 'Warrior') {
-                  return 'Tough and mighty, Warriors add +1 to Party Health';
-                } else if (playerClass == 'Wizard') {
-                  return 'Wise and perceptive, Wizards add a 10% chance of success in random events';
-                } else return 'No class is selected!';
-              })()),
-            ),
-            SizedBox(height: 50),
-            ElevatedButton(
-              onPressed: () {
-                if (usernameValue == '') {
-                  showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text("Please enter a username!"),
-                        );
-                      });
-                } else {
-                  setUsernameGuildClass();
-                  Navigator.pushNamed(context, '/homepage');
-                }
-              },
-              child: Text('Submit'),
-            ),
-          ],
+              SizedBox(height: 30),
+              Container(
+                height: 50,
+                width: 300,
+                // decoration: BoxDecoration(
+                //   border: Border.all(color: Colors.black, width: 1),
+                // ),
+                child: Text((() {
+                  if (playerClass == 'Warrior') {
+                    return 'Tough and mighty, Warriors add +1 to Party Health';
+                  } else if (playerClass == 'Wizard') {
+                    return 'Wise and perceptive, Wizards add a 10% chance of success in random events';
+                  } else return 'No class is selected!';
+                })()),
+              ),
+              SizedBox(height: 50),
+              ElevatedButton(
+                onPressed: () {
+                  if (usernameValue == '') {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            title: Text("Please enter a username!"),
+                          );
+                        });
+                  } else {
+                    setUsernameGuildClass();
+                    Navigator.pushNamed(context, '/homepage');
+                  }
+                },
+                child: Text('Submit'),
+              ),
+            ],
+          ),
         ),
       ),
     );
